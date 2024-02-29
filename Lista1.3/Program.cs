bd63a8dfa659a6df807b2152adda141c3bb2f309
FAMILIA <FAMILIA@DESKTOP-QPHAI6M>
2024-02-29 19:44:00 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1.3/Program.cs
@@ -0,0 +1,29 @@
+﻿double vorgern = 0;
+int n;
+int i = 0;
+string? damsen;
+
+do
+{
+    Console.WriteLine("Tamanho do vetor: ");
+    damsen = Console.ReadLine();
+    Int32.TryParse(damsen, out n);
+} while (n <= 0);
+double[] nester = new double[n];
+
+Console.WriteLine("");
+Console.WriteLine("Digite '0' para saír");
+Console.WriteLine("");
+
+do
+{
+    i++;
+    Console.WriteLine("Digito um numero: ");
+    damsen = Console.ReadLine();
+    Double.TryParse(damsen, out nester[i]);
+    vorgern += nester[i];
+} while (nester[i] !=0 || i == n);
+
+Console.WriteLine("Média dos valores: "+ (vorgern/(i-1)));
+
+// O valor de um homem pode ser medido pelo tamanho de sua barba, e pela grossura do seu... cinturão - olaf
